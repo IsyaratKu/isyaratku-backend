c2704004e80c00106765de65e8a89139f3ab8d97
danenherdi <danendra.herdiansyah@ui.ac.id>
2024-05-26 23:07:02 +0700

    add authentication

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const authService = require('../services/auth/auth-service');
+
+router.post('/register', authService.register);
+router.post('/login', authService.login);
+router.post('/logout', authService.logout);
+router.post('/forgot-password', authService.resetPassword);
 
 module.exports = router;
