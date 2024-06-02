ae6525b95744a80f9cd2893c31a610af46861de5
Azkaobil <azkaobilavasya@gmail.com>
2024-06-02 12:36:04 +0700

    change code in database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,20 @@
-require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
-    dialect: 'mysql'
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    dialectOptions: {
+        socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`
+    },
+    logging: false
 });
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 module.exports = sequelize;
